7d55008cf2041dd40f9b13fa236983abb4d3886b
alejo <jairosuarez651@gmail.com>
2024-04-05 18:35:16 -0400

    Laboratorio programacion curso java version 1.0

---
+++ src/domain/Peliculas.java
@@ -0,0 +1,28 @@
+package domain;
+
+public class Peliculas {
+    private String nombre;
+
+//Constructor Vacio:
+    public Peliculas(){
+    }
+    
+    //Constructor para inicalizar:
+    public Peliculas(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Peliculas{" + "nombre=" + nombre + '}';
+    }
+    
+}
---
+++ src/testExcepcion/TestExcepcion.java
@@ -0,0 +1,31 @@
+package testExcepcion;
+
+import static calculoExcepcion.CalculoExcepcion.Excepcion;
+import excepciones.OperacionExcepcion;
+
+public class TestExcepcion {
+
+    public static void main(String[] args) {
+        String datos = "";
+        try {
+            datos = Excepcion(datos);
+        } catch (OperacionExcepcion e) {
+            System.out.println("Error en acceso de datos...");
+            e.printStackTrace(System.out);
+            System.out.println(e.getMessage());
+        } catch (Exception e){
+            System.out.println("Error en Escritura de datos...");
+        
+       
+                      
+//Solo para el mensaje de error;
+            System.out.println(e.getMessage());
+            
+        } finally {
+            System.out.println("Falta pelicula por cargar");
+        }
+
+        System.out.println("Datos " + datos);
+    }
+
+}
